c316c42e0a249f5bb6cf35c81a22c453039ddbca
root <root@dwelch-desktop.(none)>
2012-09-13 23:29:06 -0400

    adding bootloader05

---
+++ bootloader05/bootloader05.c
@@ -0,0 +1,186 @@
+
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+// The raspberry pi firmware at the time this was written defaults
+// loading at address 0x8000.  Although this bootloader could easily
+// load at 0x0000, it loads at 0x8000 so that the same binaries built
+// for the SD card work with this bootloader.  Change the ARMBASE
+// below to use a different location.
+
+#define ARMBASE 0x8000
+
+extern void PUT32 ( unsigned int, unsigned int );
+extern void PUT16 ( unsigned int, unsigned int );
+extern void PUT8 ( unsigned int, unsigned int );
+extern unsigned int GET32 ( unsigned int );
+extern unsigned int GETPC ( void );
+extern void BRANCHTO ( unsigned int );
+extern void dummy ( unsigned int );
+
+extern void uart_init ( void );
+extern unsigned int uart_lcr ( void );
+extern void uart_flush ( void );
+extern void uart_send ( unsigned int );
+extern unsigned int uart_recv ( void );
+extern void hexstring ( unsigned int );
+extern void hexstrings ( unsigned int );
+extern void timer_init ( void );
+extern unsigned int timer_tick ( void );
+
+extern void timer_init ( void );
+extern unsigned int timer_tick ( void );
+
+//------------------------------------------------------------------------
+unsigned char xstring[256];
+//------------------------------------------------------------------------
+int notmain ( void )
+{
+    unsigned int ra;
+    //unsigned int rb;
+    unsigned int rx;
+    unsigned int addr;
+    unsigned int block;
+    unsigned int state;
+
+    unsigned int crc;
+
+    uart_init();
+    hexstring(0x12345678);
+    hexstring(GETPC());
+    hexstring(ARMBASE);
+    timer_init();
+
+//SOH 0x01
+//ACK 0x06
+//NAK 0x15
+//EOT 0x04
+
+//block numbers start with 1
+
+//132 byte packet
+//starts with SOH
+//block number byte
+//255-block number
+//128 bytes of data
+//checksum byte (whole packet)
+//a single EOT instead of SOH when done, send an ACK on it too
+
+    block=1;
+    addr=ARMBASE;
+    state=0;
+    crc=0;
+    rx=timer_tick();
+    while(1)
+    {
+        ra=timer_tick();
+        if((ra-rx)>=4000000)
+        {
+            uart_send(0x15);
+            rx+=4000000;
+        }
+        if((uart_lcr()&0x01)==0) continue;
+        xstring[state]=uart_recv();
+        rx=timer_tick();
+        if(state==0)
+        {
+            if(xstring[state]==0x04)
+            {
+                uart_send(0x06);
+                for(ra=0;ra<30;ra++) hexstring(ra);
+                hexstring(0x11111111);
+                hexstring(0x22222222);
+                hexstring(0x33333333);
+                uart_flush();
+                BRANCHTO(ARMBASE);
+                break;
+            }
+        }
+        switch(state)
+        {
+            case 0:
+            {
+                if(xstring[state]==0x01)
+                {
+                    crc=xstring[state];
+                    state++;
+                }
+                else
+                {
+                    //state=0;
+                    uart_send(0x15);
+                }
+                break;
+            }
+            case 1:
+            {
+                if(xstring[state]==block)
+                {
+                    crc+=xstring[state];
+                    state++;
+                }
+                else
+                {
+                    state=0;
+                    uart_send(0x15);
+                }
+                break;
+            }
+            case 2:
+            {
+                if(xstring[state]==(0xFF-xstring[state-1]))
+                {
+                    crc+=xstring[state];
+                    state++;
+                }
+                else
+                {
+                    uart_send(0x15);
+                    state=0;
+                }
+                break;
+            }
+            case 131:
+            {
+                crc&=0xFF;
+                if(xstring[state]==crc)
+                {
+                    for(ra=0;ra<128;ra++)
+                    {
+                        PUT8(addr++,xstring[ra+3]);
+                    }
+                    uart_send(0x06);
+                    block=(block+1)&0xFF;
+                }
+                else
+                {
+                    uart_send(0x15);
+                }
+                state=0;
+                break;
+            }
+            default:
+            {
+                crc+=xstring[state];
+                state++;
+                break;
+            }
+        }
+    }
+    return(0);
+}
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------
+//
+// Copyright (c) 2012 David Welch dwelch@dwelch.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------------
