2e90885bb1d639df5ddda7d5ce2fb170422372fb
C-Elegans <mtnolan2640@gmail.com>
2014-11-21 18:19:33 -0500

    35x speedup in clrScreen
    went from 223ms per call to 6.2ms

--- video01/display.c
+++ video01/display.c
@@ -21,13 +21,17 @@ static inline unsigned int max(unsigned int a, unsigned int b, unsigned int c)
 }
 
 void clrScreen(int color){
-	int i,j,address;
-	//color = color | (color << 16);
+	int i,j;
+	int register address;
+	color = color | (color << 16);
 	address=GET32(0x40040020);
-	for(i=640; i!=0;i--){
-		for(j=480; j!=0; j--){
-			PUT16(address, color);
-			address +=2;
+	for(i=480; i!=0;i--){
+		for(j=80; j!=0; j--){
+			asm volatile("str %[color],[%[address]]":: [color] "r" (color), [address] "r" (address));
+			asm volatile("str %[color],[%[address], #4]":: [color] "r" (color), [address] "r" (address));
+			asm volatile("str %[color],[%[address], #8]":: [color] "r" (color), [address] "r" (address));
+			asm volatile("str %[color],[%[address], #12]":: [color] "r" (color), [address] "r" (address));
+			address +=16;
 		}
 	}
 }
