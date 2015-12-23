1fee1784c12e9fa3d0dc7189433ce38bb65b640a
dwelch67 <dwelch@dwelch.com>
2015-12-23 03:43:12 -0500

    added a blinker01 example for the A+ and B+

--- blinker01/plus/blinker01.c
+++ blinker01/plus/blinker01.c
@@ -5,45 +5,37 @@
 extern void PUT32 ( unsigned int, unsigned int );
 extern unsigned int GET32 ( unsigned int );
 extern void dummy ( unsigned int );
-extern void HOP ( unsigned int );
 
-void put32(unsigned long int addr, unsigned long int val) {
-   volatile unsigned long int *a;
-   a = addr;
-   *a = val;
-}
+#define GPFSEL3 0x2020000C
+#define GPFSEL4 0x20200010
+#define GPSET1  0x20200020
+#define GPCLR1  0x2020002C
+
 //-------------------------------------------------------------------------
 int notmain ( void )
 {
     unsigned int ra;
 
-    ra=0x10000;
+    ra=GET32(GPFSEL4);
+    ra&=~(7<<21);
+    ra|=1<<21;
+    PUT32(GPFSEL4,ra);
 
-    PUT32(ra,0xe3a0043f); ra+=4;
-    PUT32(ra,0xe3800602); ra+=4;
-    PUT32(ra,0xe5901010); ra+=4;
-    PUT32(ra,0xe3c1160e); ra+=4;
-    PUT32(ra,0xe3811602); ra+=4;
-    PUT32(ra,0xe5801010); ra+=4;
-    PUT32(ra,0xe590100c); ra+=4;
-    PUT32(ra,0xe3c1190e); ra+=4;
-    PUT32(ra,0xe3811902); ra+=4;
-    PUT32(ra,0xe580100c); ra+=4;
-    PUT32(ra,0xe3a02902); ra+=4;
-    put32(ra,0xe3a03008); ra+=4;
-    PUT32(ra,0xe5802020); ra+=4;
-    PUT32(ra,0xe580302c); ra+=4;
-    PUT32(ra,0xe3a04601); ra+=4;
-    PUT32(ra,0xe2544001); ra+=4;
-    PUT32(ra,0x1afffffd); ra+=4;
-    PUT32(ra,0xe580202c); ra+=4;
-    PUT32(ra,0xe5803020); ra+=4;
-    PUT32(ra,0xe3a04601); ra+=4;
-    PUT32(ra,0xe2544001); ra+=4;
-    PUT32(ra,0x1afffffd); ra+=4;
-    PUT32(ra,0xeafffff4); ra+=4;
+    ra=GET32(GPFSEL3);
+    ra&=~(7<<15);
+    ra|=1<<15;
+    PUT32(GPFSEL3,ra);
 
-    HOP(0x10000);
+    while(1)
+    {
+        PUT32(GPSET1,1<<(47-32));
+        PUT32(GPCLR1,1<<(35-32));
+        for(ra=0;ra<0x100000;ra++) dummy(ra);
+        PUT32(GPCLR1,1<<(47-32));
+        PUT32(GPSET1,1<<(35-32));
+        for(ra=0;ra<0x100000;ra++) dummy(ra);
+    }
+
     return(0);
 }
 //-------------------------------------------------------------------------
@@ -52,7 +44,7 @@ int notmain ( void )
 
 //-------------------------------------------------------------------------
 //
-// Copyright (c) 2012 David Welch dwelch@dwelch.com
+// Copyright (c) 2015 David Welch dwelch@dwelch.com
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
