ecb1559309f396fa2f3f6ef6cf26f53e97337848
dwelch <dwelch>
2016-03-16 13:29:49 -0400

    starting to separate each board time into its own directory tree.

---
+++ pizero/blinker02/blinker02.c
@@ -0,0 +1,69 @@
+
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+extern void PUT32 ( unsigned int, unsigned int );
+extern unsigned int GET32 ( unsigned int );
+extern void dummy ( unsigned int );
+
+#define SYSTIMERCLO 0x20003004
+#define GPFSEL3 0x2020000C
+#define GPFSEL4 0x20200010
+#define GPSET1  0x20200020
+#define GPCLR1  0x2020002C
+
+//0x01000000 17 seconds
+//0x00400000 4 seconds
+//#define TIMER_BIT 0x01000000
+#define TIMER_BIT 0x00400000
+
+//-------------------------------------------------------------------------
+int notmain ( void )
+{
+    unsigned int ra;
+
+    ra=GET32(GPFSEL4);
+    ra&=~(7<<21);
+    ra|=1<<21;
+    PUT32(GPFSEL4,ra);
+
+    //ra=GET32(GPFSEL3);
+    //ra&=~(7<<15);
+    //ra|=1<<15;
+    //PUT32(GPFSEL3,ra);
+
+
+    while(1)
+    {
+        PUT32(GPSET1,1<<(47-32));
+        //PUT32(GPCLR1,1<<(35-32));
+        while(1)
+        {
+            ra=GET32(SYSTIMERCLO);
+            if((ra&=TIMER_BIT)==TIMER_BIT) break;
+        }
+        PUT32(GPCLR1,1<<(47-32));
+        //PUT32(GPSET1,1<<(35-32));
+        while(1)
+        {
+            ra=GET32(SYSTIMERCLO);
+            if((ra&=TIMER_BIT)==0) break;
+        }
+    }
+    return(0);
+}
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------
+//
+// Copyright (c) 2012 David Welch dwelch@dwelch.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------------
