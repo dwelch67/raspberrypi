eb8aaccfaf3e80305c1d8652d050face4e594212
dwelch <dwelch@dwelch.com>
2016-03-27 13:00:14 -0400

    blinker01 example for pi2 in svc mode

---
+++ boards/pi2/SVC/blinker01/blinker01.c
@@ -0,0 +1,61 @@
+
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+extern void PUT32 ( unsigned int, unsigned int );
+extern unsigned int GET32 ( unsigned int );
+extern void dummy ( unsigned int );
+extern unsigned int GETCPSR ( void );
+
+#define GPFSEL3 0x3F20000C
+#define GPFSEL4 0x3F200010
+#define GPSET1  0x3F200020
+#define GPCLR1  0x3F20002C
+
+//-------------------------------------------------------------------------
+int notmain ( void )
+{
+    unsigned int ra;
+    unsigned int rb;
+
+    ra=GET32(GPFSEL4);
+    ra&=~(7<<21);
+    ra|=1<<21;
+    PUT32(GPFSEL4,ra);
+
+    ra=GET32(GPFSEL3);
+    ra&=~(7<<15);
+    ra|=1<<15;
+    PUT32(GPFSEL3,ra);
+
+    ra=GETCPSR();
+    if((ra&0x1F)==0x1A) rb=0x20000;
+    else                rb=0x10000;
+
+    while(1)
+    {
+        PUT32(GPSET1,1<<(47-32));
+        PUT32(GPCLR1,1<<(35-32));
+        for(ra=0;ra<rb;ra++) dummy(ra);
+        PUT32(GPCLR1,1<<(47-32));
+        PUT32(GPSET1,1<<(35-32));
+        for(ra=0;ra<rb;ra++) dummy(ra);
+    }
+
+    return(0);
+}
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------
+//
+// Copyright (c) 2015 David Welch dwelch@dwelch.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------------
