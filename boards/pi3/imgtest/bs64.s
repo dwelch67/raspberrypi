9a3334995208ee5751efbd2283d494b05e1c3b68
root <dwelch@dwelch.com>
2017-01-18 21:35:52 -0500

    an experiment to see the kernel8.img, kernel7.img differences in action

---
+++ boards/pi3/imgtest/bs64.s
@@ -0,0 +1,48 @@
+
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+
+.globl _start
+_start:
+    mov sp,#0x80000
+    bl notmain
+hang: b hang
+
+.globl PUT32
+PUT32:
+  str w1,[x0]
+  ret
+
+.globl GET32
+GET32:
+    ldr w0,[x0]
+    ret
+
+.globl GETPC
+GETPC:
+    mov x0,x30
+    ret
+
+.globl GETCPSR
+GETCPSR:
+    mov x0,#0
+    ret
+
+.globl dummy
+dummy:
+    ret
+
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+//
+// Copyright (c) 2012 David Welch dwelch@dwelch.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------
---
+++ boards/pi3/imgtest/notmain.c
@@ -0,0 +1,138 @@
+
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+
+// 2  outer corner
+// 4
+// 6
+// 8  TX out
+// 10 RX in
+
+extern void PUT32 ( unsigned int, unsigned int );
+extern unsigned int GET32 ( unsigned int );
+extern unsigned int GETPC ( void );
+extern void dummy ( unsigned int );
+extern unsigned int GETCPSR ( void );
+//-------------------------------------------------------------------
+
+#define PBASE 0x3F000000
+
+extern void PUT32 ( unsigned int, unsigned int );
+extern unsigned int GET32 ( unsigned int );
+
+#define ARM_TIMER_CTL   (PBASE+0x0000B408)
+#define ARM_TIMER_CNT   (PBASE+0x0000B420)
+
+#define GPFSEL1         (PBASE+0x00200004)
+#define GPSET0          (PBASE+0x0020001C)
+#define GPCLR0          (PBASE+0x00200028)
+#define GPPUD           (PBASE+0x00200094)
+#define GPPUDCLK0       (PBASE+0x00200098)
+
+#define AUX_ENABLES     (PBASE+0x00215004)
+#define AUX_MU_IO_REG   (PBASE+0x00215040)
+#define AUX_MU_IER_REG  (PBASE+0x00215044)
+#define AUX_MU_IIR_REG  (PBASE+0x00215048)
+#define AUX_MU_LCR_REG  (PBASE+0x0021504C)
+#define AUX_MU_MCR_REG  (PBASE+0x00215050)
+#define AUX_MU_LSR_REG  (PBASE+0x00215054)
+#define AUX_MU_MSR_REG  (PBASE+0x00215058)
+#define AUX_MU_SCRATCH  (PBASE+0x0021505C)
+#define AUX_MU_CNTL_REG (PBASE+0x00215060)
+#define AUX_MU_STAT_REG (PBASE+0x00215064)
+#define AUX_MU_BAUD_REG (PBASE+0x00215068)
+
+//-------------------------------------------------------------------
+void uart_send ( unsigned int c )
+{
+    while(1)
+    {
+        if(GET32(AUX_MU_LSR_REG)&0x20) break;
+    }
+    PUT32(AUX_MU_IO_REG,c);
+}
+//-------------------------------------------------------------------
+void uart_flush ( void )
+{
+    while(1)
+    {
+        if((GET32(AUX_MU_LSR_REG)&0x100)==0) break;
+    }
+}
+//-------------------------------------------------------------------
+void hexstrings ( unsigned int d )
+{
+    //unsigned int ra;
+    unsigned int rb;
+    unsigned int rc;
+
+    rb=32;
+    while(1)
+    {
+        rb-=4;
+        rc=(d>>rb)&0xF;
+        if(rc>9) rc+=0x37; else rc+=0x30;
+        uart_send(rc);
+        if(rb==0) break;
+    }
+    uart_send(0x20);
+}
+//-------------------------------------------------------------------
+void hexstring ( unsigned int d )
+{
+    hexstrings(d);
+    uart_send(0x0D);
+    uart_send(0x0A);
+}
+//-------------------------------------------------------------------
+void uart_init ( void )
+{
+    unsigned int ra;
+
+    PUT32(AUX_ENABLES,1);
+    PUT32(AUX_MU_IER_REG,0);
+    PUT32(AUX_MU_CNTL_REG,0);
+    PUT32(AUX_MU_LCR_REG,3);
+    PUT32(AUX_MU_MCR_REG,0);
+    PUT32(AUX_MU_IER_REG,0);
+    PUT32(AUX_MU_IIR_REG,0xC6);
+    PUT32(AUX_MU_BAUD_REG,270);
+    ra=GET32(GPFSEL1);
+    ra&=~(7<<12); //gpio14
+    ra|=2<<12;    //alt5
+    ra&=~(7<<15); //gpio15
+    ra|=2<<15;    //alt5
+    PUT32(GPFSEL1,ra);
+    PUT32(GPPUD,0);
+    for(ra=0;ra<150;ra++) dummy(ra);
+    PUT32(GPPUDCLK0,(1<<14)|(1<<15));
+    for(ra=0;ra<150;ra++) dummy(ra);
+    PUT32(GPPUDCLK0,0);
+    PUT32(AUX_MU_CNTL_REG,3);
+}
+//-------------------------------------------------------------------
+int notmain ( void )
+{
+    uart_init();
+    hexstring(0x12345678);
+    hexstring(DEFINEME);
+    hexstring(GETPC());
+    hexstring(GETCPSR());
+    hexstring(0x12345678);
+    return(0);
+}
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------
+//
+// Copyright (c) 2014 David Welch dwelch@dwelch.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------
