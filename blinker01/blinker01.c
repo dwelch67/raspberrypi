cb26650de310857c7cd95807f894e79a4ed39c58
huaiyang <huaiyang@users.noreply.github.com>
2023-07-14 17:05:15 +0800

    Update blinker01.c

--- blinker01/blinker01.c
+++ blinker01/blinker01.c
@@ -9,6 +9,7 @@ extern void dummy ( unsigned int );
 #define GPFSEL1 0x20200004
 #define GPSET0  0x2020001C
 #define GPCLR0  0x20200028
+#define Test 0x2222223
 
 //-------------------------------------------------------------------------
 int notmain ( void )
